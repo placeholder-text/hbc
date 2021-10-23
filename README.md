096c2cb92dec0737746b0b249e88493284f8e919
sample-text-here <56274831+sample-text-here@users.noreply.github.com>
2021-10-22 18:21:14 -0700

    Add `find`, `every`, `some` methods.

--- README.md
+++ README.md
@@ -6,4 +6,7 @@ to creating new ones.
 **current features:**
 - `.map` for Map and Set
 - `.filter` for Map and Set
+- `.find` for Map and Set
+- `.every` for Map and Set
+- `.some` for Map and Set
 
--- test.js
+++ test.js
@@ -6,6 +6,13 @@ const set = new Set().add("foo").add("bar");
 
 deepEqual(map.map(i => i + 1), new Map().set("foo", 2).set("bar", 3));
 deepEqual(map.filter(i => i === 1), new Map().set("foo", 1));
+deepEqual(map.find(i => i === 1), "foo");
+deepEqual(map.some(i => i === 1), true);
+deepEqual(map.every(i => i === 1), false);
+
 deepEqual(set.map(i => i.toUpperCase()), new Set().add("FOO").add("BAR"));
 deepEqual(set.filter(i => i === "foo"), new Set().add("foo"));
+deepEqual(set.find(i => i === "foo"), "foo");
+deepEqual(set.some(i => i === "foo"), true);
+deepEqual(set.every(i => i === "foo"), false);
 
