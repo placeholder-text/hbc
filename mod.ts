d2606cb88ade4a2444160b3ef41183b665614475
QmelZ <qmelzthevoid@gmail.com>
2021-10-23 19:48:09 +0300

    Add hacky loader for deno so we can use require.

--- mod.ts
+++ mod.ts
@@ -1,4 +1,22 @@
-import "./core/map.js";
-import "./core/set.js";
-import "./core/array.js";
+async function require(filename: string){
+	try{
+		return eval(
+			await Deno.emit(filename, {
+				bundle: "classic",
+				check: false,
+				compilerOptions: {
+					module: "commonjs",
+					checkJs: false
+				}
+			}).then(e => e.files["deno:///bundle.js"])
+		);
+	}catch{
+		return {};
+	}
+}
 
+await Promise.all([
+	require("./core/map.js"),
+	require("./core/set.js"),
+	require("./core/array.js")
+]);
