486260faf6d66b7a699ce7e6af17b66eae1915c3
QmelZ <qmelzthevoid@gmail.com>
2021-10-24 14:06:23 +0300

    More features & fixes for generator.

--- core/generator.js
+++ core/generator.js
@@ -1,19 +1,45 @@
 const Generator = (function*(){})().constructor;
 
 // always return a boolean, not undefined
-Object.defineProperty(Generator.prototype, "done", {
-	get() { return this._done ?? false; } 
-});
+Generator.prototype.done = function done(){
+	return this._done ?? false;
+}
 
 const next = Generator.prototype.next;
-Generator.prototype.next = function() {
-	const got = next.call(this);
+Generator.prototype.next = function next(...args) {
+	const got = this._values.length - 1 > this._index ?
+		this._values[this._index + 1] :
+		next.call(this, ...args);
+	
 	this._done = got.done;
+	this._current = got.value;
+	
+	this._values.push(got);
+	this._index++;
+	
 	return got;
 };
 
-Generator.prototype.forEach = function(callback, thisArg) {
-	if(thisArg) callback = callback.bind(thisArg);
-	for(let i of this) callback(i.value);
-};
+Generator.prototype.nextValue = function nextValue(...args){
+	return this.next(...args).value;
+}
 
+Generator.prototype.peek = function peek(){
+	return this._values[this._values.length - 1];
+}
+
+Generator.prototype._values = [];
+Generator.prototype._index = 0;
+Generator.prototype.prev = function prev(){
+	return this._values[--this._index];
+}
+
+Generator.prototype.forEach = function forEach(callback, thisArg) {
+	if(thisArg) callback = callback.bind(thisArg);
+	for(let i of this){
+		this._values.push(i);
+		this._index++;
+		
+		callback(i.value, i.done);
+	}
+};
