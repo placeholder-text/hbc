63013bec0f16e43396a328ccddf805309c20ff1b
QmelZ <qmelzthevoid@gmail.com>
2021-10-24 17:42:24 +0300

    Import the files so they work.

--- mod.cjs
+++ mod.cjs
@@ -2,4 +2,4 @@ require("./core/map.js");
 require("./core/set.js");
 require("./core/array.js");
 require("./core/generator.js");
-
+require("./core/timers.js");
--- mod.js
+++ mod.js
@@ -2,4 +2,4 @@ import "./core/map.js";
 import "./core/set.js";
 import "./core/array.js";
 import "./core/generator.js";
-
+import "./core/timers.js";
